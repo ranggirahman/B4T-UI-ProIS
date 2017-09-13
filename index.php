12a2d836a0cab46de81a2749acae45630999b4f7
ranggirahman <ranggirahman@gmail.com>
2017-09-13 10:17:23 +0700

    Proyek B4T
    Proyek Sistem Informasi B4T

---
+++ index.php
@@ -0,0 +1,94 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="img/favicon.ico">
+
+    <title>Balai Besar Bahan dan Barang Teknik</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/material-icons.css" rel="stylesheet">
+    <link href="css/jumbotron.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="index.php"><img src="img/logo.png" class="img-fluid" style="max-width: 20%; and height: auto" alt="Responsive image"> Balai Besar Bahan dan Barang Teknik</a>        
+      </div>
+    </nav>
+
+    <!-- Main -->
+    <div class="jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <h1 class="display-3">Sistem Informasi Pengadaan Barang dan Jasa</h1>
+          </div>
+          <div class="col-md-1">        
+          </div>
+          <div class="col-md-3" style="padding-top: 20px">            
+            <form action="" method="POST">
+              <table border=0>
+                <th><h3>Login</h3></th>
+                <tr height="50px">
+                    <td><input class="form-control" type="user" name="email" placeholder="Username" maxlength="40" size="40" required></td>
+                </tr>
+                <tr height="50px">
+                    <td><input class="form-control" type="password" name="password" placeholder="Password" maxlength="40" size="40" required></td>
+                </tr>
+              </table>
+              <hr>
+              <table border=0>
+                <tr>
+                  <td><input class="btn btn-success" type="submit" name="submit" value="Login"></td>
+                  <td><input class="btn btn-warning" type="submit" name="submit" value="Lupa Password"></td>
+                </tr>                            
+              </table>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-md-4">
+          <h2>Footer</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+        </div>
+        <div class="col-md-4">
+          <h2>Footer</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+        </div>
+        <div class="col-md-4">
+          <h2>Footer</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+        </div>
+      </div>
+
+      <hr>
+
+      <footer>
+        <p>&copy; 2017 Balai Besar Bahan dan Barang Teknik (B4T). All Rights Reserved</p>
+      </footer>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/jquery.min.js"></script>
+  </body>
+</html>
---
+++ list.php
@@ -0,0 +1,138 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="img/favicon.ico">
+
+    <title>Balai Besar Bahan dan Barang Teknik</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->    
+    <link href="css/material-icons.css" rel="stylesheet">
+    <link href="css/dashboard.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="index.php"><img src="img/logo.png" class="img-fluid" style="max-width: 20%; and height: auto" alt="Responsive image"> Balai Besar Bahan dan Barang Teknik</a>
+        <a>
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="material-icons" style="font-size: 13px">perm_identity</i> User</button>
+        </a>       
+      </div>
+    </nav>
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="container" style="padding-top: 100px">
+      <div class="row">
+
+        <main class="ml-sm-auto col-md-12" role="main">
+          <section class="row text-center placeholders">
+            <div class="col-6 col-sm-3 placeholder">
+              <img src="data:image/gif;base64,R0lGODlhAQABAIABAAJ12AAAACwAAAAAAQABAAACAkQBADs=" width="200" height="200" class="img-fluid rounded-circle" alt="Generic placeholder thumbnail">
+              <h4>Total Pesanan</h4>
+            </div>
+            <div class="col-6 col-sm-3 placeholder">
+              <img src="data:image/gif;base64,R0lGODlhAQABAIABAADcgwAAACwAAAAAAQABAAACAkQBADs=" width="200" height="200" class="img-fluid rounded-circle" alt="Generic placeholder thumbnail">
+              <h4>Menunggu Disetujui</h4>
+            </div>
+            <div class="col-6 col-sm-3 placeholder">
+              <img src="data:image/gif;base64,R0lGODlhAQABAIABAAJ12AAAACwAAAAAAQABAAACAkQBADs=" width="200" height="200" class="img-fluid rounded-circle" alt="Generic placeholder thumbnail">
+              <h4>Proses Pengadaan</h4>
+            </div>
+            <div class="col-6 col-sm-3 placeholder">
+              <img src="data:image/gif;base64,R0lGODlhAQABAIABAADcgwAAACwAAAAAAQABAAACAkQBADs=" width="200" height="200" class="img-fluid rounded-circle" alt="Generic placeholder thumbnail">
+              <h4>Tidak Disetujui</h4>
+            </div>
+          </section>
+
+          <div class="table-responsive" style="padding-top: 20px">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th>
+                    <button class="btn btn-success" type="submit"><i class="material-icons" style="font-size: 15px">add</i> Buat Pesanan</button>
+                  </th>                  
+                  <form class="form-inline">
+                    <th>
+                      <input class="form-control" type="text" placeholder="Cari Berkas" size="100%" aria-label="Search">
+                    </th>
+                    <th>
+                      <button class="btn btn-primary" type="submit"><i class="material-icons" style="font-size: 13px">search</i></button>
+                    </th>
+                  </form>
+                </tr>              
+              </thead>         
+            </table> 
+          </div>
+
+          <div class="table-responsive">
+            <table class="table table-striped">
+              <thead class="thead-inverse">
+                <tr>
+                  <th>Pemesan</th>
+                  <th>Deskripsi Pesanan</th>
+                  <th>Tanggal</th>
+                  <th>Monitor</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr style="font-weight:bold">
+                  <td>Ranggi Rahman</td>
+                  <td>Donec id elit non mi porta gravida at eget metus. Fusce dapibus</td>
+                  <td>7 Sep 2017</td>
+                  <td>Usulan Dalam Antrian</td>
+                </tr>
+                <tr style="font-weight:bold">
+                  <td>Ahmad Zainal A</td>
+                  <td>Donec id elit non mi porta gravida at eget metus. Fusce dapibus</td>
+                  <td>5 Sep 2017</td>
+                  <td>Usulan Dalam Antrian</td>
+                </tr>     
+                <tr style="">
+                  <td>Wiwi Juwita</td>
+                  <td>Donec id elit non mi porta gravida at eget metus. Fusce dapibus</td>
+                  <td>3 Sep 2017</td>
+                  <td>Usulan Diacc TU</td>
+                </tr>  
+                <tr style="">
+                  <td>Sita Kartina</td>
+                  <td>Donec id elit non mi porta gravida at eget metus. Fusce dapibus</td>
+                  <td>2 Sep 2017</td>
+                  <td>Usulan Diacc Kabid</td>
+                </tr>  
+                <tr style="">
+                  <td>Risna</td>
+                  <td>Donec id elit non mi porta gravida at eget metus. Fusce dapibus</td>
+                  <td>1 Sep 2017</td>
+                  <td>Usulan Diacc PPK</td>
+                </tr>  
+                <tr style="">
+                  <td>Maerin Novita</td>
+                  <td>Donec id elit non mi porta gravida at eget metus. Fusce dapibus</td>
+                  <td>1 Sep 2017</td>
+                  <td>Dalam Proses Pengadaan</td>
+                </tr>  
+              </tbody>
+            </table>
+          </div>
+        </main>
+      </div>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/jquery.min.js"></script>
+  </body>
+</html>
