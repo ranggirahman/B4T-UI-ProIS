ca04ddc07ad59e868bafdaa6caebb341873e57ad
ranggirahman <ranggirahman@gmail.com>
2017-09-18 11:31:29 +0700

    add fview, edit prof

--- edit_profile.php
+++ edit_profile.php
@@ -40,96 +40,41 @@
 
     <div class="container" style="padding-top: 70px">
       <div class="card">
-        <h4 class="card-header">Form Daftar Pemesanan Barang / Jasa</h4>
+        <h4 class="card-header">Edit Profile</h4>
         <div class="card-body">
           <form action='response.php' method='POST'>
             <table class="table borderless">
               <tr>
-                <td width="20%">Kepada</td>
-                <td colspan="2"><input class="form-control" type="text" name="kepada" placeholder="Kepala B4T,u.p. Kepala Bagian Tata Usaha" readonly></td>
-                <td width="30%"></td>
+                <td width="15%">Username</td>
+                <td ><input class="form-control" type="text" name="username" placeholder="Username" readonly></td>
+                <td></td>
+                <td width="20%"></td>
               </tr>
               <tr>
-                <td>Dari</td>
-                <td colspan="2"><input class="form-control" type="text" name="dari" placeholder="Ranggi Rahman (Seksi Informasi)" data-toggle="popover" title="" data-content="Sesuai ID Login Anda" readonly></td>
-                <td width="30%"></td>
+                <td>Ganti Nama</td>
+                <td><input class="form-control" type="text" name="Nama" required></td>
+                <td></td>
+                <td></td>
               </tr>
               <tr>
-                <td>Agar</td>
-                <td colspan="2"><input class="form-control" type="text" name="agar" required></td>
-                <td width="30%"></td>
-              </tr>
-              <tr>
-                <td>Diperlukan Tanggal</td>
-                <td><input class="form-control" type="date" name="tanggal" required></td>
-                <td width="50%"></td>
-                <td width="30%"></td>
-              </tr>                   
-            </table>
-            <hr>
-            <div class="table-responsive">
-              <table border="0" class="table">
-                <thead class="thead-default">
-                  <tr>
-                    <th>No.</th>
-                    <th width="17%">Kategori</th>
-                    <th>Nama dan Spesifikasi Barang / Jasa</th>
-                    <th width="20%" colspan="2">Banyaknya</th>
-                  </tr>
-                </thead>
-                <tbody>   
-                  <tr style="">
-                    <td>1</td>
-                    <td>ATK</td>
-                    <td>Donec id elit non mi porta gravida at eget metus</td>
-                    <td colspan="2">3 Buah</td>
-                  </tr>  
-                  <tr style="">
-                    <td>2</td>
-                    <td>ATK</td>
-                    <td>Donec id elit non mi porta gravida at eget metus</td>
-                    <td colspan="2">2 Set</td>
-                  </tr>  
-                  <tr style="">
-                    <td>3</td>
-                    <td>ATK</td>
-                    <td>Donec id elit non mi porta gravida at eget metus</td>
-                    <td colspan="2">1 Buah</td>
-                  </tr>  
-                  <tr style="">
-                    <td>4</td>
-                    <td>ATK</td>
-                    <td>Donec id elit non mi porta gravida at eget metus</td>
-                    <td colspan="2">1 Pack</td>
-                  </tr>
-                  <tr style="">
-                    <td></td>
-                    <td><input class="form-control" type="text" name="kategori" required></td>
-                    <td><input class="form-control" type="text" name="nbarangjasa" required></td>
-                    <td><input class="form-control" type="text" name="banyak" required></td>
-                    <td><button class="btn btn-info" type="button">Add</button></td>
-                  </tr>    
-                </tbody>
-              </table>
-            </div>
-            <hr>
-            <div class="form-group">
-              <label for="keterangan">Keterangan</label>
-              <textarea class="form-control" rows="5" id="keterangan" required></textarea>
-            </div>
+                <td>Ganti Password</td>
+                <td><input class="form-control" type="password" name="ps1" placeholder="Masukan password baru" required></td>
+                <td><input class="form-control" type="password" name="ps2" placeholder="Masukan password baru lagi" required></td>
+              </tr>                               
+            </table>      
           </form>          
         </div>
         <div class="card-footer">
           <table border=0>
             <tr>
               <td style="padding-right: 20px">
-                Masukan Kembali Password Akun Anda
+                Masukan Kembali Password Lama Anda
               </td>
               <td style="padding-right: 20px">
                 <input class="form-control" type="password" name="password" required>
               </td>
               <td>            
-                <a href="#" class="btn btn-success" data-toggle="modal" data-target="#exampleModal">Submit</a>
+                <a href="#" class="btn btn-success" data-toggle="modal" data-target="#exampleModal">Save</a>
               </td>
             </tr>
           </table>
@@ -164,14 +109,9 @@
     <script src="js/bootstrap.min.js" type="text/javascript"></script>
 
     <script type="text/javascript">
-      //modal
+      // modal
       $('#myModal').on('shown.bs.modal', function () {
         $('#myInput').focus()
-      })
-
-      //pop up mini
-      $(function () {
-        $('[data-toggle="popover"]').popover()
       })
     </script>
 
---
+++ form_view.php
@@ -0,0 +1,163 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="img/favicon.ico">
+
+    <title>Balai Besar Bahan dan Barang Teknik</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->    
+    <link href="css/material-icons.css" rel="stylesheet">
+    <link href="css/dashboard.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+  </head>
+
+
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="index.php"><img src="img/logo.png" class="img-fluid" style="max-width: 20%; and height: auto" alt="Responsive image"> Balai Besar Bahan dan Barang Teknik</a>
+        <div class="dropdown">
+          <button class="btn btn-info dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="material-icons" style="font-size: 13px">account_circle</i> Username</button>
+          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="#">Edit Profile</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" style="color: red; font-weight: bold" href="#">Logout</a>
+          </div>
+        </div>       
+      </div>
+    </nav>      
+      </div>
+    </nav>
+
+    <div class="container" style="padding-top: 70px">
+      <div class="card">
+        <h4 class="card-header">Daftar Pemesanan Barang / Jasa</h4>
+        <div class="card-body">
+          <table class="table borderless">
+            <tr>
+              <td width="20%">Kepada</td>
+              <td colspan="2">Kepala B4T,u.p. Kepala Bagian Tata Usaha</td>
+              <td width="30%"></td>
+            </tr>
+            <tr>
+              <td>Dari</td>
+              <td colspan="2">Ranggi Rahman (Seksi Informasi)</td>
+              <td width="30%"></td>
+            </tr>
+            <tr>
+              <td>Agar</td>
+              <td colspan="2">Diadakan</td>
+              <td width="30%"></td>
+            </tr>
+            <tr>
+              <td>Diperlukan Tanggal</td>
+              <td colspan="2">18 Desember 2017</td>
+              <td width="30%"></td>
+            </tr>                   
+          </table>
+          <hr>
+          <div class="table-responsive">
+            <table border="0" class="table">
+              <thead class="thead-default">
+                <tr>
+                  <th>No.</th>
+                  <th width="17%">Kategori</th>
+                  <th>Nama dan Spesifikasi Barang / Jasa</th>
+                  <th width="20%">Banyaknya</th>
+                  <th>Acc</th>
+                </tr>
+              </thead>
+              <tbody>   
+                <tr style="">
+                  <td>1</td>
+                  <td>ATK</td>
+                  <td>Donec id elit non mi porta gravida at eget metus</td>
+                  <td>3 Buah</td>
+                  <td><i class="material-icons" style="font-size: 20px; color: green">check_circle</i></td>
+                </tr>  
+                <tr style="">
+                  <td>2</td>
+                  <td>ATK</td>
+                  <td>Donec id elit non mi porta gravida at eget metus</td>
+                  <td>2 Set</td>
+                  <td><i class="material-icons" style="font-size: 20px; color: green">check_circle</i></td>
+                </tr>  
+                <tr style="">
+                  <td>3</td>
+                  <td>ATK</td>
+                  <td>Donec id elit non mi porta gravida at eget metus</td>
+                  <td>1 Buah</td>
+                  <td><i class="material-icons" style="font-size: 20px; color: green">check_circle</i></td>
+                </tr>  
+                <tr style="">
+                  <td>4</td>
+                  <td>ATK</td>
+                  <td>Donec id elit non mi porta gravida at eget metus</td>
+                  <td>1 Pack</td>
+                  <td><i class="material-icons" style="font-size: 20px; color: green">check_circle</i></td>
+                </tr>   
+              </tbody>
+            </table>
+          </div>
+          <hr>
+          <label for="keterangan">Keterangan</label>
+          <div class="card">
+            <div class="card-body">Implementasi Industri 4.0 pada Industri Komponen Otomotif di Indonesia</div>
+          </div>
+          <hr>
+          <table>
+            <tr>
+              <td>Permintaan diterima oleh Ka.Bag Tata Usaha :</td>
+              <td>11 September 2017</td>
+            </tr>
+          </table>
+          <label for="keterangan"><b>Rekomendasi / Catatan Ka.Bag Tata Usaha</b></label>
+          <div class="card">
+            <div class="card-body">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+          </div>
+          <hr>
+          <label for="keterangan"><b>Perintah / Catatan Pejabat Pembuat Komitmen</b></label>
+          <div class="card">
+            <div class="card-body">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. </div>
+          </div>  
+        </div>        
+      </div>
+      <br>
+      <br>
+      <br>
+      <br>
+    </div> <!-- /container -->
+
+    <!-- Modal -->
+    <div class="modal fade" style="padding-top: 200px" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Form Belum Disi Dengan Benar </h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    
+    <script src="js/jquery-1.12.4.min.js" type="text/javascript"></script> 
+    <script src="js/popper.min.js" type="text/javascript"></script> 
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
+
+  </body>
+</html>
